eaa2e09d4910910471f59b48fb12c6a82c82e1c1
Marcelo Perini <67985323+marceloprni@users.noreply.github.com>
2023-10-11 08:46:45 -0300

    evolução 5
    capitulo 8 acabado

--- NODE_VR3/index.js
+++ NODE_VR3/index.js
@@ -4,10 +4,15 @@ const array = [4,6,2,8,3,5,9,6,1];
 
 console.log('Imprimindo de 3 em 3');
 
-_.chunk(array,4).forEach((item) => {
+_.chunk(array,3).forEach((item) => {
     console.log('-----');
 
     item.forEach((i) => {
         console.log(i + "!");
     })
 });
+
+_.defaults({ 'a': 1 }, { 'a': 3, 'b': 2 });
+// → { 'a': 1, 'b': 2 }
+_.partition([1, 2, 3, 4], n => n % 2);
+// → [[1, 3], [2, 4]]
